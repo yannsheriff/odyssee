7ecf5df8dd7ec9896c575ce38dea44bd80bb89df
Yannis Cherif <yannischerif@MacBook-Pro-de-Yannis.local>
2018-05-18 13:51:17 +0200

    save

--- src/components/Island/narration/index.js
+++ src/components/Island/narration/index.js
@@ -28,18 +28,63 @@ import styles from './styles'
 export default class Narration extends Component {
 
   constructor(props) {
-    super(props)
+    super(props);
+    this.animation = []
     this.state = {
-      text: this.props.snippet.text
+      isTransitionFinished: true,
+      transitionDuration: this.props.transitionDuration ? this.props.transitionDuration : 5000,
+      texts: [
+        {
+          // If animation is not null put it in the state
+          text: props.snippet.text ? props.snippet.text : null,
+          position: new Animated.Value(0),
+        }
+      ],
     }
   }
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.snippet.text !== this.state.text) {
-      this.setState({ text: nextProps.snippet.text })
+
+  componentWillReceiveProps (nextProps) {
+    if(nextProps.snippet.text && this.state.isTransitionFinished ) { // If component receive a new animation 
+      
+      // Get transition duration
+      let transitionDuration = nextProps.transitionDuration ? nextProps.transitionDuration : this.state.transitionDuration
+      
+      // Push new animation in state array
+      this.setState({ 
+        isTransitionFinished: false,
+        texts:[ 
+          {
+            text: nextProps.snippet.text, 
+            position: new Animated.Value(screen.width-2)
+          }, 
+          ...this.state.texts
+        ]
+      }, function () { // When set state did finish
+
+
+        // Animate the old & the animation to the left
+        Animated.parallel([
+          Animated.timing(this.state.texts[1].position, {
+            duration: transitionDuration ,
+            toValue: -screen.width + 2, 
+          }),
+          Animated.timing(this.state.texts[0].position, {
+            duration: transitionDuration,
+            toValue: 0,
+          }),
+        ]).start(()=> {
+          // When the animation is finished wiat 1s and delete old animation from the state Array
+          setTimeout(()=> {
+            this.setState({ 
+              texts: [this.state.animations.shift()],
+              isTransitionFinished: true
+            })
+          }, 500)
+        }); 
+      })
     }
   }
-
 
 
   render() {
