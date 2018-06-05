a989ee04e0247792864ed34c977a0926e465e0d4
yannsheriff <yanncherif@live.fr>
2018-06-05 17:52:39 +0200

    pop up

--- src/components/Main-menu/index.js
+++ src/components/Main-menu/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Animated, Easing, View, Text, StyleSheet, Button,TouchableOpacity, Image } from "react-native";
 import LottieView from "lottie-react-native";
 import { connect } from "react-redux";
-import { toggleMenu, saveMenu } from "../../redux/actions/menu";
+import { toggleMenu, saveMenu, navigateTo } from "../../redux/actions/menu";
 import screen from "../../helpers/ScreenSize";
 import Achivements from "./Achievements-page";
 import { BlurView } from "react-native-blur";
@@ -21,9 +21,10 @@ class MainMenu extends React.Component {
     this.state = {
       reduxStore: this.props.store,
       paginationPosition: new Animated.Value(0),
+      display: false,
+      popupDisplay: true,
       _toggleMenu: this.props.toggleMenu,
       _saveMenu: this.props.saveMenu,
-      display: false
     };
   }
 
@@ -64,6 +65,10 @@ class MainMenu extends React.Component {
     this.page = id
   };
 
+  quitIsland() {
+
+  }
+
 
   closeMenu = () => {
     ReactNativeHaptic.generate('impact')
@@ -89,50 +94,73 @@ class MainMenu extends React.Component {
               blurType="light"
               blurAmount={20}
             />
-            <View
-              style={styles.close}
-            >
-              <TouchableOpacity 
-                onPress={this.closeMenu} 
-                style={{width: 30, height: 30}}
+            {renderIf( !this.state.popupDisplay, 
+            <View>
+              <View
+                style={styles.close}
               >
-                <Image 
+                <TouchableOpacity 
+                  onPress={this.closeMenu} 
                   style={{width: 30, height: 30}}
-                  source={images.closeMainMenu} 
-                  />
-              </TouchableOpacity>
-            </View>
-            <Swiper
-              style={styles.wrapper}
-              showsPagination={false}
-              onIndexChanged={this.indexDidChange}
-              index={this.page}
-            >
-              <View style={styles.slide1}>
-                <Text style={styles.text}>Hello Swiper</Text>
+                >
+                  <Image 
+                    style={{width: 30, height: 30}}
+                    source={images.closeMainMenu} 
+                    />
+                </TouchableOpacity>
               </View>
-              <View style={{ backgroundColor: "transparent" }}>
-                <Achivements  />
+              <Swiper
+                style={styles.wrapper}
+                showsPagination={false}
+                onIndexChanged={this.indexDidChange}
+                index={this.page}
+              >
+                <View style={styles.slide1}>
+                  <Text style={styles.text}>Hello Swiper</Text>
+                </View>
+                <View style={{ backgroundColor: "transparent" }}>
+                  <Achivements  />
+                </View>
+                <View style={styles.slide3}>
+                  <Text style={styles.text}>And simple</Text>
+                </View>
+              </Swiper>
+              <View
+                style={styles.navigationLine}
+              >
+                <Animated.View
+                  style={[styles.navigationIndicator, {left: this.state.paginationPosition,}]}
+                />
+                <View style={styles.categories}> 
+                  <Text style={[styles.categorieMenu, { opacity: this.state.page === 0 ? 1 : 0.7 }]}>Carte</Text>
+                  <View style={[styles.dash, {marginLeft: -10}]} />
+                  <Text style={[styles.categorieMenu, { opacity: this.state.page === 1 ? 1 : 0.7 }]}>Inventaire</Text>
+                  <View style={[styles.dash, {marginRight: -9}]}/>
+                  <Text style={[styles.categorieMenu, { opacity: this.state.page === 2 ? 1 : 0.7 }]}>Reglages</Text>
+                </View>
+              </View> 
+            </View>)}
+
+            {renderIf( this.state.popupDisplay, 
+              <View style={{width: screen.width, marginTop: screen.height / 100 * 40, flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+                <Text style={{textAlign: "center", color: "white", fontSize: 22, fontFamily: "Infini-Regular",width: screen.width /100 * 90 }} > Etes-vous sur de vouloir quitter l'ile ? </Text>
+                <View style={{marginTop: 30, flexDirection: "row", justifyContent: "space-around", width: screen.width / 100 * 60 }}>
+                  <TouchableOpacity
+                    onPress={() => { this.state._navigateTo('Sailing') }}
+                  >
+                   <Text style={{textAlign: "center", color: "white", fontSize: 22, fontFamily: "Infini-Regular"}} > Oui </Text>
+                   </TouchableOpacity>
+                  <TouchableOpacity
+                    onPress={() => { this.setState({ popupDisplay: false })}}
+                  >
+                    <Text style={{textAlign: "center", color: "white", fontSize: 22, fontFamily: "Infini-Regular"}} > Non </Text>
+                  </TouchableOpacity>
+                </View>
               </View>
-              <View style={styles.slide3}>
-                <Text style={styles.text}>And simple</Text>
-              </View>
-            </Swiper>
-            <View
-              style={styles.navigationLine}
-            >
-              <Animated.View
-                style={[styles.navigationIndicator, {left: this.state.paginationPosition,}]}
-              />
-              <View style={styles.categories}> 
-                <Text style={[styles.categorieMenu, { opacity: this.state.page === 0 ? 1 : 0.7 }]}>Carte</Text>
-                <View style={[styles.dash, {marginLeft: -10}]} />
-                <Text style={[styles.categorieMenu, { opacity: this.state.page === 1 ? 1 : 0.7 }]}>Inventaire</Text>
-                <View style={[styles.dash, {marginRight: -9}]}/>
-                <Text style={[styles.categorieMenu, { opacity: this.state.page === 2 ? 1 : 0.7 }]}>Reglages</Text>
-              </View>
-            </View>
-          </View>
+
+            )}
+            
+          </View> 
         )}
       </View>
     );
@@ -156,6 +184,9 @@ const mapDispatchToProps = dispatch => {
     },
     saveMenu: (state) => {
       dispatch(saveMenu(state));
+    },
+    navigateTo: (screen) => {
+      dispatch(navigateTo(screen))
     }
   };
 };
